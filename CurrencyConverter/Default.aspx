ce06cfd7a130db6843fbb681d2bc57597f9d7c34
John Bass <jmbass36@gmail.com>
2024-08-08 12:37:53 -0500

    finished lab

--- CurrencyConverter/Default.aspx
+++ CurrencyConverter/Default.aspx
@@ -9,7 +9,39 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Convert:    "></asp:Label>
+            <asp:TextBox ID="TxtUSD" runat="server" Width="177px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Label ID="Label2" runat="server" Text="USD to:      "></asp:Label>
+&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem Selected="True">Select a Currency</asp:ListItem>
+                <asp:ListItem Value="1">Euro</asp:ListItem>
+                <asp:ListItem Value="2">Canadian</asp:ListItem>
+                <asp:ListItem Value="3">Japense Yen</asp:ListItem>
+            </asp:DropDownList>
+            <br />
+            <br />
+            <br />
+            <asp:Button ID="BtnOk" runat="server" OnClick="BtnOk_Click" Text="OK" Width="84px" />
+&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="BtnGraph" runat="server" OnClick="BtnGraph_Click" Text="Show Graph" Width="86px" />
+            <br />
+            <br />
         </div>
+        <asp:Image ID="ImgGraph" runat="server" Height="171px" ImageUrl="images/blank.jpg" Width="291px" />
+        <br />
+        <br />
+        <asp:Label ID="LblMessage" runat="server"></asp:Label>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
     </form>
 </body>
 </html>
--- CurrencyConverter/Default.aspx.cs
+++ CurrencyConverter/Default.aspx.cs
@@ -13,5 +13,57 @@ namespace CurrencyConverter
         {
 
         }
+
+        protected void BtnOk_Click(object sender, EventArgs e)
+        {
+            LblMessage.Text = "";
+            if(TxtUSD.Text == "" || DropDownList1.Text == "")
+            {
+                LblMessage.Text = "Please enter a value to be converted or select a currecy to convert to";
+                return;
+            }
+            double amount = double.Parse(TxtUSD.Text);
+            double can = 1.37;
+            double yen = 147.20;
+            double euro = 0.92;
+            double result;
+            if(DropDownList1.Text == "1")
+            {
+                result = amount * euro;
+                LblMessage.Text = $"${amount}.00 USD equals ${result.ToString("0.00")} Euro";
+            }
+            else if(DropDownList1.Text == "2")
+            {
+                result = amount * can;
+                LblMessage.Text = $"${amount}.00 USD equals ${result.ToString("0.00")} Canadian Dollars";
+            }
+            else if(DropDownList1.Text == "3")
+            {
+                result = amount * yen;
+                LblMessage.Text = $"${amount}.00 USD equals ${result.ToString("0.00")} Japanese Yen";
+            }
+        }
+
+        protected void BtnGraph_Click(object sender, EventArgs e)
+        {
+            ImgGraph.ImageUrl = "images/blank.jpg";
+            if (TxtUSD.Text == "" || DropDownList1.Text == "")
+            {
+                LblMessage.Text = "Please enter a value to be converted or select a currecy to convert to";
+                return;
+            }
+            if (DropDownList1.Text == "1")
+            {
+                ImgGraph.ImageUrl = "images/UStoEuro.png";
+            }
+            else if (DropDownList1.Text == "2")
+            {
+                ImgGraph.ImageUrl = "images/UsDtoCan.png";
+            }
+            else if (DropDownList1.Text == "3")
+            {
+                ImgGraph.ImageUrl = "images/USDtoYen.png";
+            }
+        }
     }
 }
